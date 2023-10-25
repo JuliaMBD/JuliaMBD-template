f72d99b662175d37f1921e2e0a54d8ae5b406ef8
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2023-10-25 10:19:45 +0900

    commit

--- requirements.jl
+++ requirements.jl
@@ -2,3 +2,4 @@ using Pkg
 Pkg.add("Plots")
 Pkg.add("DifferentialEquations")
 Pkg.add(url="https://github.com/JuliaReliab/LookupTable.jl.git")
+Pkg.add(url="https://github.com/JuliaMBD/JuliaMBD.jl.git")
