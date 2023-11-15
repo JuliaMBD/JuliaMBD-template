99cbe05d22daf7eb7bcd7695052beadbaa31a93b
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2023-11-15 01:22:57 +0000

    update

--- requirements.jl
+++ requirements.jl
@@ -1,6 +1,8 @@
 using Pkg
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("EzXML")
 Pkg.add("Plots")
 Pkg.add("DifferentialEquations")
-Pkg.add("EzXML")
 Pkg.add(url="https://github.com/JuliaReliab/LookupTable.jl.git")
 Pkg.add(url="https://github.com/JuliaMBD/JuliaMBD.jl.git")
