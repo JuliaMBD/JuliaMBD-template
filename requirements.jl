9598f19ae79e04fe6aa2042f35dcb3fc0e4c16ff
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2023-11-15 01:44:40 +0000

    update

--- requirements.jl
+++ requirements.jl
@@ -6,3 +6,6 @@ Pkg.add("Plots")
 Pkg.add("DifferentialEquations")
 Pkg.add(url="https://github.com/JuliaReliab/LookupTable.jl.git")
 Pkg.add(url="https://github.com/JuliaMBD/JuliaMBD.jl.git")
+
+## download latest block library
+download("https://raw.githubusercontent.com/JuliaMBD/JuliaMBD.jl/main/xml/JuliaMBD.xml", "JuliaMBD.xml")
